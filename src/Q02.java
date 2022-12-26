30b6b8018f1f0bd5f6732d763281c778a23cd3ed
Berkan <sonmezayberkan@gmail.com>
2022-12-26 20:22:45 +0300

    26.12.2022

---
+++ src/Q02.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Q02 {
+
+    public static void main(String[] args) {
+     //2 Boyutlu bir Multidimensional array i input kabul edip, arraydeki tüm elemanları bir
+     //listeye kopyalayan ve harf sırasına göre yazdıran bir METHOD yazınız.
+     //Eg : Input : {{Ali,Veli,Ayse},{Hasan,Can},{Suzan}}
+     //   Output:[Ali, Ayse, Can, Hasan, Suzan, Veli]
+    	
+    	String arr[][]={{"Ali","Veli","Ayse"},{"Hasan","Can"},{"Suzan"}};
+    	listEkle(arr);
+
+    }
+
+	private static void listEkle(String[][] arr) {
+		ArrayList<String> list1=new ArrayList<String>();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				list1.add(arr[i][j]);
+			}
+		}
+		Collections.sort(list1);
+		System.out.println(list1);
+		
+	}
+
+}
---
+++ src/Q09.java
@@ -0,0 +1,33 @@
+public class Q09 {
+    public static void main(String[] args) {
+
+
+    /*  Write a Java program to get a String from user as input and find the maximumCounts occurring character in that string.
+         (Ignore case sensitivity)
+
+         Kullanicidan aldiginiz bir string icinde en cok tekrar eden karakteri yazdiriniz
+       input : Learning java is easy
+       output: maximumCounts occurring character is : a
+        */
+    	String cumle = "Learning java is easy";
+		String arr[] = cumle.split("");
+		int count = 0;
+		int enCok = 0;
+		char enCokHarf = ' ';
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i].contains(arr[j])) {
+					count++;
+				}
+
+			}
+			if (count > enCok) {
+				enCok = count;
+				enCokHarf = arr[i].charAt(0);
+			}
+			count = 0;
+		}
+		System.out.println("maximum occurring character is : "+enCokHarf);
+    }
+}
+
